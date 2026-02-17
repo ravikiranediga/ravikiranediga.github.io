a5b24f8d5307d3e9330f7a3e8d62e8295898fc14
ravikiranediga <ravikirangowd91.com>
2026-02-17 18:21:31 +0530

    Update config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RAVIKIRAN.github.io/',
+  base: '/',
   server: {
     port: 3000,
     host: true
